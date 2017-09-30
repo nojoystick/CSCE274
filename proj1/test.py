787fdb94d71756c3f382dc993fc171d4325e95c7
WARD <curtisaw@cocsce-l1d39-01.ds.sc.edu>
2017-09-30 17:36:23 -0400

    updated test.py so now it completes the pentagon!!!!!!!!!! Only problem is that the angle needs to be tested more in depth.

--- proj1/test.py
+++ proj1/test.py
@@ -1,6 +1,27 @@
 import serial
 import state_interface
 import connection_interface
+from time import sleep
+L = 235 #distance wheels are apart in mm
+
+def straightTime(velocity, distance):
+  time = (float(distance*10))/float(velocity)
+  return time
+
+def turnTime(velocity, angle):
+  omega = float(2*velocity)/float(L)
+  time = angle/omega
+  return time
 
 connection = state_interface.Interface()
-connection.drive(-200,500)
+for i in range(0,5):
+  time = straightTime(200,32)
+  connection.drive(200,0)
+  sleep(time)
+
+  time = turnTime(200,1.225)
+  connection.drive(200, -1)
+  sleep(time)
+
+connection.stop()
+connection.close()
