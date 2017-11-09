e50b5ec1c5c6887321eb88d041b4376cc688bc04
dallinw <dallinjwilliams@gmail.com>
2017-11-09 17:54:56 -0500

    Test script for wall reading

---
+++ proj3/testing_files/wall_test.py
@@ -0,0 +1,12 @@
+import state_interface
+self.state = state_interface.Interface()
+
+front_right = state.read_light_front_right()
+center_right = state.read_light_center_right()
+right = state.read_light_right()
+
+print "FRONT RIGHT: "+front_right
+print "CENTER RIGHT: "+center_right
+print "RIGHT: "+center_right
+
+
