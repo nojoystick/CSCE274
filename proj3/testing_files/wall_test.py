c2003860db7e5946b91b59cb295ce70d3d1ed4a5
WARD <curtisaw@cocsce-l1d39-01.ds.sc.edu>
2017-11-10 18:06:54 -0500

    Fixed up wall test

--- proj3/testing_files/wall_test.py
+++ proj3/testing_files/wall_test.py
@@ -5,8 +5,8 @@ front_right = state.read_light_front_right()
 center_right = state.read_light_center_right()
 right = state.read_light_right()
 
-print "FRONT RIGHT: "+front_right
-print "CENTER RIGHT: "+center_right
-print "RIGHT: "+center_right
+print "FRONT RIGHT: "+str(front_right)
+print "CENTER RIGHT: "+str(center_right)
+print "RIGHT: "+str(center_right)
 
 
